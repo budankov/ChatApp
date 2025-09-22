8348ecf9ddefae845fbf84f0ec4f43b3b2db6fef
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2025-09-23 00:30:58 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Привіт Костя!</Text>
+      <Text style={styles.title}>Hello World!</Text>
     </View>
   );
 };
